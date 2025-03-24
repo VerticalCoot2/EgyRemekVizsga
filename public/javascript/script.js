38c4b8e3f3e918d5b69c56c6af087248412b9940
verticalcoot2 <verticalcoot2@gmail.com>
2025-03-24 16:23:00 +0100

    comit

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -24,27 +24,27 @@ function build(target)
         ]
     };
 
-    for(let i = 0;i < data.food.length ;i++)
+    for(let i = 0;i < 100 ;i++)
     {
         let patya = document.createElement("div");
             patya.classList.add("kaja");
             let nev = document.createElement("div");
                 nev.classList.add("kajaText");
-                nev.innerHTML = data.food[i].nev;
+                nev.innerHTML = data.food[0].nev;
             
             let kaloria = document.createElement("div");
                 kaloria.classList.add("kajaText");
-                kaloria.innerHTML = data.food[i].kaloria + "kcal";
+                kaloria.innerHTML = data.food[0].kaloria + "kcal";
 
             let tipus = document.createElement("div");
                 tipus.classList.add("kajaText");
-                tipus.innerHTML = data.food[i].etkezes_típus;
+                tipus.innerHTML = data.food[0].etkezes_típus;
             
-            let linyk = document.createElement("div");
+            let linyk = document.createElement("div");//link
                 let linykH = document.createElement("a");
                     linykH.classList.add("kajaText");
-                    linykH.href = data.food[i].recept_link;
-                    linykH.innerHTML = "recept";
+                    linykH.href = data.food[1].recept_link;
+                    linykH.innerHTML = "Recept";
                     linykH.target = "_blank";
                 linyk.appendChild(linykH);
             
