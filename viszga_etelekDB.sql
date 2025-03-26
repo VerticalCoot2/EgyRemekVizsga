5aa531c59bef78b3a17b2f19a8fdcb18e6821ef1
KBotond0223 <95409532+KBotond0223@users.noreply.github.com>
2025-03-26 19:34:42 +0100

    gecire elegem van

--- viszga_etelekDB.sql
+++ viszga_etelekDB.sql
@@ -1,20 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.27-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             12.10.0.7000
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 -- Dumping database structure for eteldb
 CREATE DATABASE IF NOT EXISTS `eteldb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `eteldb`;
@@ -14201,9 +14184,6 @@ INSERT INTO `vizsgadb` (`id`, `Name`, `Calories`, `Fat_g_`, `Protein_g_`, `Carbo
 	(14162, 'Wheat Bread As Ingredient In Sandwiches', 263, 3.960000, 11.450000, 45.290000, '5.18', '5.4', '76.046'),
 	(14163, 'Sauce As Ingredient In Hamburgers', 271, 22.850000, 1.290000, 17.190000, '13.02', '0.6', '73.801'),
 	(14164, 'Industrial Oil As Ingredient In Food', 892, 100.000000, 0.000000, 0.000000, '0', '0', '22.422');
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
