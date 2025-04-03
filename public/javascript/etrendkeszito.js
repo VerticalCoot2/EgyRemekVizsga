b8773b4afc8895abad239d189e8dbadcd2be41fb
verticalcoot2 <verticalcoot2@gmail.com>
2025-04-03 15:46:55 +0200

    select2gud

--- public/javascript/etrendkeszito.js
+++ public/javascript/etrendkeszito.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function()
 {
-    build(document.getElementById("patya"))
-    buildSelect(document.getElementById("select2s"));
+    build(document.getElementById("patya"));
+    //buildSelect(document.getElementById("select2s"));
 
     document.getElementById("menuButton").addEventListener("mousedown", function(event)
     {
         event.preventDefault();
         window.open("/", "_self");
     });
+    $('.js-example-basic-single').select2();
 });
-
 async function szelektAll()
 {
     let response = (await fetch("/api/selectAll",
@@ -19,79 +19,75 @@ async function szelektAll()
     return (await response).json();
 }
 
-async function buildSelect(target)
-{
-    target.innerHTML = null;
-    data = await szelektAll();
-    for(let i = 0; i < data.length; i++)
-    {
-        let opt = document.createElement("option");
-        opt.value = data[i].id;
-        opt.innerHTML = data[i].Name;
-        target.appendChild(opt);
-    }
-}
-
-$(document).ready(function() {
-    $('.js-example-basic-multiple').select2();
-});
+// async function buildSelect(target)
+// {
+//     target.innerHTML = null;
+//     data = await szelektAll();
+//     for(let i = 0; i < data.length; i++)
+//     {
+//         let opt = document.createElement("option");
+//         opt.value = data[i].id;
+//         opt.innerHTML = data[i].Name;
+//         target.appendChild(opt);
+//     }
+// }
 
 async function build(target)
-{
+{let patya = document.getElementById("patya");
+            patya.classList.add("kaja");
     target.innerHTML = null;
-    data = await szelektAll();
+    let data = await szelektAll();
     console.log(await data.length)
     for(let i = 0;i < data.length ;i++)
     {
-        let patya = document.createElement("div");
-            patya.classList.add("kaja");
+        
 
-            let Name = document.createElement("div");
+            let Name = document.createElement("option");
                 Name.classList.add("kajaText");
-                Name.innerHTML = data[i].Name;
+                Name.innerHTML = data[i].Name+" "+ data[i].Calories + "kcal" + " " + data[i].Fat_g_ + " " + data[i].Protein_g_ + " " + data[i].Carbohydrate_g_ + " " + data[i].Sugars_g_ + " " + data[i].Fiber_g_ + " " + data[i]._200_Calorie_Weight_g_;
             
-            let Calories = document.createElement("div");
-                Calories.classList.add("kajaText");
-                Calories.innerHTML = data[i].Calories + "kcal";
+            // let Calories = document.createElement("option");
+            //     Calories.classList.add("kajaText");
+            //     Calories.innerHTML = 
 
-            let Fat_g = document.createElement("div");
-                Fat_g.classList.add("kajaText");
-                Fat_g.innerHTML = data[i].Fat_g_;
+            // let Fat_g = document.createElement("option");
+            //     Fat_g.classList.add("kajaText");
+            //     Fat_g.innerHTML = ;
             
-            let Protein = document.createElement("div");
-                Protein.classList.add("kajaText");
-                Protein.innerHTML = data[i].Protein_g_;
+            // let Protein = document.createElement("option");
+            //     Protein.classList.add("kajaText");
+            //     Protein.innerHTML = ;
             
-            let Carb_g = document.createElement("div");
-                Carb_g.classList.add("kajaText");
-                Carb_g.innerHTML = data[i].Carbohydrate_g_;
+            // let Carb_g = document.createElement("option");
+            //     Carb_g.classList.add("kajaText");
+            //     Carb_g.innerHTML = ;
 
-            let Sugars_g = document.createElement("div");
-                Sugars_g.classList.add("kajaText");
-                Sugars_g.innerHTML = data[i].Sugars_g_;
+            // let Sugars_g = document.createElement("option");
+            //     Sugars_g.classList.add("kajaText");
+            //     Sugars_g.innerHTML = ;
             
-            let Fiber_g = document.createElement("div");
-                Fiber_g.classList.add("kajaText");
-                Fiber_g.innerHTML = data[i].Fiber_g_;
+            // let Fiber_g = document.createElement("option");
+            //     Fiber_g.classList.add("kajaText");
+            //     Fiber_g.innerHTML = ;
             
-            let cal2 = document.createElement("div");
-                cal2.classList.add("kajaText");
-                cal2.innerHTML = data[i]._200_Calorie_Weight_g_;
+            // let cal2 = document.createElement("option");
+            //     cal2.classList.add("kajaText");
+            //     cal2.innerHTML = ;
             
             
             
-            patya.appendChild(Name);
-            patya.appendChild(Calories);
-            patya.appendChild(Fat_g);
-            patya.appendChild(Protein);
-            patya.appendChild(Carb_g);
-            patya.appendChild(Sugars_g);
-            patya.appendChild(Fiber_g);
-            patya.appendChild(cal2);
+            target.appendChild(Name);
+            // patya.appendChild(Calories);
+            // patya.appendChild(Fat_g);
+            // patya.appendChild(Protein);
+            // patya.appendChild(Carb_g);
+            // patya.appendChild(Sugars_g);
+            // patya.appendChild(Fiber_g);
+            // patya.appendChild(cal2);
             
 
                 
         
-        target.appendChild(patya);
+        // target.appendChild(patya);
     }
 }
