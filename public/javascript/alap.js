c6783c22c98f9eed370a8162108b816d632e936d
Gpatrik5 <patrikuser555@gmail.com>
2025-04-07 13:29:55 +0200

    patyaaaa

--- public/javascript/alap.js
+++ public/javascript/alap.js
@@ -1,19 +1,4 @@
 document.addEventListener("DOMContentLoaded", function()
 {
-    document.getElementById("foodButton").addEventListener("mousedown", function(event)
-    {
-        event.preventDefault();
-        window.open("/etrendkeszito", "_self");
-    });
-    let titel = document.getElementById("tojgli");
-    titel.innerHTML = "DietInator"
-    tojgli.addEventListener("mouseenter", function()
-    {
-       this.innerHTML = "Patya"; 
-    });
-    tojgli.addEventListener("mouseleave", function()
-    {
-       this.innerHTML = "DietInator";
-    });
 
 });
