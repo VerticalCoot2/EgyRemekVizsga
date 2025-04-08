326c15111b1aa689d9bad03106c1b0c9605495d5
verticalcoot2 <verticalcoot2@gmail.com>
2025-04-08 22:51:04 +0200

    kaloriaszamlalo_upload_tamper

--- public/javascript/kaloriaszamlalo.js
+++ public/javascript/kaloriaszamlalo.js
@@ -1,9 +1,36 @@
 document.addEventListener("DOMContentLoaded", function()
 {
     document.getElementById("insert_gomb").addEventListener("click", async function()
-{
-    await insert_fetch();
-})
+    {
+        let inputs = document.getElementsByClassName("inputs");
+        
+        let check = 0;
+        let approved = true
+        while(check != inputs.length && approved)
+        {
+            if(inputs[check].value != "" && inputs[check].value != null)
+            {
+                check++;
+            }
+            else
+            {
+                approved = false;
+            }
+        }
+        if(approved)
+        {
+            await insert_fetch();
+            for(let i = 0; i < inputs.length; i++)
+            {
+                inputs[i].value = null;
+            }
+            alert("Upload request successfull! It will be supervised shortly.");
+        }
+        else
+        {
+            alert("You have no filled it out correctly!");
+        }
+    });
 });
 
 async function insert_fetch()
