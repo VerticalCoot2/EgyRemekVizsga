37d29b3294934b291f881a5dfa1cf73d8fe1acbf
Kbotond0223 <kbotond2004@gmail.com>
2025-04-10 10:49:39 +0200

    rendes admin(patya)

--- adminInsert.sql
+++ adminInsert.sql
@@ -2,11 +2,11 @@
 CREATE TABLE `adminInsert` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `Name` varchar(255) NOT NULL,
-  `Calories` int(11) NOT NULL,
-  `Fat_g_` int(11) NOT NULL,
-  `Protein_g_` int(11) NOT NULL,
-  `Carbohydrate_g_` int(11) NOT NULL,
-  `Sugars_g_` int(11) NOT NULL,
-  `Fiber_g_` int(11) NOT NULL,
-  `_200_Calorie_Weight_g_` int(11) NOT NULL
+  `Calories` smallint(6) DEFAULT NULL,
+  `Fat_g_` decimal(20,6) DEFAULT NULL,
+  `Protein_g_` decimal(20,6) DEFAULT NULL,
+  `Carbohydrate_g_` decimal(20,6) DEFAULT NULL,
+  `Sugars_g_` varchar(10) DEFAULT NULL,
+  `Fiber_g_` varchar(10) DEFAULT NULL,
+  `_200_Calorie_Weight_g_` varchar(10) DEFAULT NULL
 ) 
