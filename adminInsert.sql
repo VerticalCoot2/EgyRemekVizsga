7310503197349ec270aa54cc0c6d0cf0520cb428
Kbotond0223 <kbotond2004@gmail.com>
2025-04-10 10:51:17 +0200

    admin oks

--- adminInsert.sql
+++ adminInsert.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `adminInsert` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `Name` varchar(255) NOT NULL,
-  `Calories` smallint(6) DEFAULT NULL,
+  `Calories` smallint(6) NOT NULL,
   `Fat_g_` decimal(20,6) DEFAULT NULL,
   `Protein_g_` decimal(20,6) DEFAULT NULL,
   `Carbohydrate_g_` decimal(20,6) DEFAULT NULL,
