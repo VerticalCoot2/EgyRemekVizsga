a6ab334dc5fcde28d334d7c605e8dcb5a8ba8cd2
verticalcoot2 <verticalcoot2@gmail.com>
2025-04-14 14:29:28 +0200

    sulekKUKI

--- public/javascript/etrendkeszito.js
+++ public/javascript/etrendkeszito.js
@@ -467,15 +467,18 @@ function cardGen(data, target)
             {
                 card.classList.add("cardEaten");
                 card.classList.add("oszlop");
-                if(data.dine != null)
-                {
-                    let mire = document.createElement("div");
-                        mire.innerHTML = "For " + data.dine;
-                    bottom.appendChild(mire);
-                }
-                let mennyit = document.createElement("div");
-                    mennyit.innerHTML = "Amount eaten(g): " + data.amountG;
-                    bottom.appendChild(mennyit);
+                let holderDiv = document.createElement("div");
+                holderDiv.classList.add("holderDiv");
+                    if(data.dine != null)
+                    {
+                        let mire = document.createElement("div");
+                            mire.innerHTML = "For " + data.dine;
+                        holderDiv.appendChild(mire);
+                    }
+                    let mennyit = document.createElement("div");
+                        mennyit.innerHTML = "Amount eaten(g): " + data.amountG;
+                        holderDiv.appendChild(mennyit);
+                bottom.appendChild(holderDiv);
 
                 let tr = document.createElement("tr");
                     let th = document.createElement("th");
